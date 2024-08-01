307b551e9e5b6459494f635eede8dc8f9e4e413a
ozkayaoguz <oguzdt@gmail.com>
2024-08-01 22:22:48 +0300

    - tmux nvim esc delay fix - nvim undodir path fix

--- nvim/.config/nvim/lua/ozkayaoguz/set.lua
+++ nvim/.config/nvim/lua/ozkayaoguz/set.lua
@@ -18,7 +18,7 @@ vim.opt.wrap = false
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") or os.getenv("USERPROFILE") .. "/.vim/undodir"
+vim.opt.undodir = (os.getenv("HOME") or os.getenv("USERPRFILE")) .. "/.vim/undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = false
