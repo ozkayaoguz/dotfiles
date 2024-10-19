67f5398f1d9cd1c484bcb84b275490a76de84edd
ozkayaoguz <oguzdt@gmail.com>
2024-10-20 01:25:24 +0300

    gitsigns config update

--- nvim/.config/nvim/lua/ozkayaoguz/plugins/git.lua
+++ nvim/.config/nvim/lua/ozkayaoguz/plugins/git.lua
@@ -9,10 +9,13 @@ return {
         "lewis6991/gitsigns.nvim",
         config = function()
             require("gitsigns").setup({
+                signs_staged_enable = false,
                 current_line_blame = true,
+                attach_to_untracked = true,
             })
 
             vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>")
+            vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>")
             vim.keymap.set("n", "<leader>gn", ":Gitsigns next_hunk<CR>")
             vim.keymap.set("n", "<leader>gp", ":Gitsigns prev_hunk<CR>")
         end,
