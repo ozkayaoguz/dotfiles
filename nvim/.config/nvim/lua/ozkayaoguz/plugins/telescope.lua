08e8f451e62f6b25d732a0ceb4ce414729c6e583
ozkayaoguz <oguzdt@gmail.com>
2024-10-30 22:05:57 +0300

    telescope fzf

--- nvim/.config/nvim/lua/ozkayaoguz/plugins/telescope.lua
+++ nvim/.config/nvim/lua/ozkayaoguz/plugins/telescope.lua
@@ -16,6 +16,24 @@ return {
         end,
     },
     {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release",
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    fzf = {
+                        fuzzy = true,
+                        override_generic_sorter = true,
+                        override_file_sorter = true,
+                        case_mode = "smart_case",
+                    },
+                },
+            })
+
+            require("telescope").load_extension("fzf")
+        end,
+    },
+    {
         "nvim-telescope/telescope-ui-select.nvim",
         config = function()
             require("telescope").setup({
@@ -30,6 +48,7 @@ return {
                     },
                 },
             })
+
             require("telescope").load_extension("ui-select")
         end,
     },
